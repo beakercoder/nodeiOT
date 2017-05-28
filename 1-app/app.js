a12a51f91c679c321dfbfe45e8964e72280b3b68
darktowerjk <darktowerjk@gmail.com>
2017-05-28 16:24:57 -0400

    Update app.js

--- 1-app/app.js
+++ 1-app/app.js
@@ -17,9 +17,6 @@ var Datastore = require('@google-cloud/datastore');
 const projectId = "doolinhomeiot";
 const datasetId = "OfficeData";
 const tableId = "iotdata";
-//const bigquery = BigQuery({
-//  projectId: projectId
-//});
 
 // Instantiate a datastore client
 var datastore = Datastore();
@@ -36,71 +33,69 @@ var PUBSUB_VERIFICATION_TOKEN = process.env.PUBSUB_VERIFICATION_TOKEN;
 var topic = pubsub.topic(process.env.PUBSUB_TOPIC);
 
 var subscription = pubsub.subscription(process.env.PUBSUB_SUBSCRIPTION_NAME);
-// Instantiates a client
-  var request = {projectId,datasetId,tableId,resource: {"kind": "bigquery#tableDataInsertAllRequest","rows": 
-    [
-      {
-        'insertId': 123456,
-        'json': '{'nameid': 123,'messagedata':'test1'}'
-      }
-    ]},
-    // Auth client
-    auth: authClient
-  };
+google.auth.getApplicationDefault(function(err, authClient) {
+    if (err) {
+        console.log('Authentication failed because of ', err);
+        return;
+    }
+    if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+        var scopes = ['https://www.googleapis.com/auth/cloud-platform'];
+        authClient = authClient.createScoped(scopes);
+    }
+//'insertId': 123456, 'json': '{"nameid": 123,"messagedata":"test1"
+
+//var request = {projectId: projectId,datasetId: datasetId,tableID: tableId,resource: {"kind": "bigquery#tableDataInsertAllRequest","rows":
+//    {}, auth: authClient]};
+
 // [END setup]
 
-function storeEvent(message) 
+function storeEvent(message)
 {
     var key = datastore.key('officedata');
-	key.identifier = "id"
+    key.identifier = "id"
     // You can uncomment some of the other things if you want to store them in the database
 
     var obj = {
-		messageid: message.id
-		//deviceid: message.attributes.device_id,
-		////event: message.attributes.event,
-		//published: message.attributes.published_at,
-	    	//data: message.data
-	}
+        messageid: message.id
+        //deviceid: message.attributes.device_id,
+        ////event: message.attributes.event,
+        //published: message.attributes.published_at,
+        //data: message.data
+    }
     datastore.save(
-    {
-        key: key,
-        data: obj
-    }, function(err) 
-    {
-		if(err) 
-		{
-			console.log('There was an error storing the event', err);
-		}
-		console.log('stored in datastore', obj);
-    }
+        {
+            key: key,
+            data: obj
+        }, function(err)
+        {
+            if(err)
+            {
+                console.log('There was an error storing the event', err);
+        }
+    console.log('stored in datastore', obj);
+        }
     );
-bigquery.tabledata.insertAll(request, function(err, result) 
-    {
-    if (err) 
-    {
-        return console.error(err);
-    }
-        console.log(result);
-    }
-);
+    //save to BigQuery Tables
+    bigquery.tabledata.insertAll({auth: oath2Client,'projectId': projectId, 'datasetId': datasetId, 'tableId': tableId, 'resource': {"kind": "bigquery#tableDataInsertAllRequest","rows":[{"insertId": 123456, "json": {"nameid": "123","messagedata":"test1"}}]}, func);
+    
+        //err: result});
 
-}
+
 
 subscription.on('message', function(message) {
-	console.log('event received', message);
-	// Called every time a message is received.
-	// message.id = ID used to acknowledge its receival.
-	// message.data = Contents of the message.
-	// message.attributes = Attributes of the message.
-	storeEvent(message);
-	message.ack();
+    console.log('event received', message);
+    // Called every time a message is received.
+    // message.id = ID used to acknowledge its receival.
+    // message.data = Contents of the message.
+    // message.attributes = Attributes of the message.
+    storeEvent(message);
+    message.ack();
 });
 
 // [START listen]
 var server = app.listen(process.env.PORT || 8080, function () {
-  console.log('App listening on port %s', server.address().port);
-  console.log('Press Ctrl+C to quit.');
+    console.log('App listening on port %s', server.address().port);
+    console.log('Press Ctrl+C to quit.');
 });
 // [END listen]
 // [END app]
